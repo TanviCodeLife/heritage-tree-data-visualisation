d8c08ed0e246689a0ed4acd45e20d35e1df94224
Paige Williams and Tanvi Garg <tanvi@>
2019-01-17 10:31:44 -0800

    Add geoJSON to map.

--- src/main.js
+++ src/main.js
@@ -8,7 +8,22 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-
+// function treeData() {
+//  let treePromiseObject = new TreePromise();
+//  let treePromise = treePromiseObject.getTrees();
+//
+//  treePromise.then(function(response){
+//    let body = JSON.parse(response);
+//    let data = new TreeData();
+//    for(let i = 0; i < body.features.length; i++){
+//      let treesLat = body.features[i].geometry.coordinates[0];
+//      let treesLong = body.features[i].geometry.coordinates[1];
+//
+//      data.addTreeToTreeData(treesLat, treesLong);
+//    }
+//
+//  })
+// }
 
 function addMarker(latitute, longitude, contentLoc, map){
   const latLng = new google.maps.LatLng(latitute, longitude);
@@ -27,9 +42,25 @@ function addMarker(latitute, longitude, contentLoc, map){
 
 
 $(document).ready(function(){
-  let options = { key: process.env.API_KEY };
+  // let coords = treeData()
+  let treePromiseObject = new TreePromise();
+  let treePromise = treePromiseObject.getTrees();
   let map;
-  loadGoogleMapsApi(options).then(function (googleMaps) {
+  treePromise.then(function(response){
+    let body = JSON.parse(response);
+    let data = new TreeData();
+    for(let i = 0; i < body.features.length; i++){
+      let treesLat = body.features[i].geometry.coordinates[0];
+      let treesLong = body.features[i].geometry.coordinates[1];
+
+      data.addTreeToTreeData(treesLat, treesLong);
+    }
+    let options = { key: process.env.API_KEY };
+    console.log(data)
+    return loadGoogleMapsApi(options)
+  })
+
+  .then(function (googleMaps) {
   map = new googleMaps.Map(document.querySelector('#map'), {
     center: {
       lat: 45.520840,
@@ -37,58 +68,18 @@ $(document).ready(function(){
     },
     zoom: 12
   })
-  const epicodus = { lat: 45.520840, lng: -122.677366 };
-  const marker1 = new googleMaps.Marker({ position: epicodus, map: map });
-    var markers = [{
-      "locName": "IRCO",
-      "street": "10301 N.E. Glisan Street",
-      "city": "Portland",
-      contentString: "<p>IRCO</p>" + "<p>10301 N.E. Glisan Street</p>" + "<a class='btn btn-primary' href='https://s3-us-west-2.amazonaws.com/wecode-file-storage-development-objectstoreaf1a2ace/pdfs/kindergarten-signup.pdf'>Details!</a>",
-      "state": "OR",
-      "zip": 97220,
-      lat: 45.52687,
-      lon: -122.556986
-    },
-    {
-      "locName": "Earl Boyles Elementary School",
-      "street": "10822 S.E. Bush Street",
-      "city": "Portland",
-      contentString: "<p>Earl Boyles Elementary School</p>" + "<p>10822 S.E. Bush Street</p>" + "<a class='btn btn-primary' href='https://s3-us-west-2.amazonaws.com/wecode-file-storage-development-objectstoreaf1a2ace/pdfs/kindergarten-signup.pdf'>Details!</a>",
-      "state": "OR",
-      "zip": 97266,
-      lat: 45.494709,
-      lon: -122.551952
-    }];
 
-    markers.forEach(marker => {
-      addMarker(marker.lat, marker.lon, marker.contentString, map);
-    });
+    map.data.loadGeoJson(
+      'https://opendata.arcgis.com/datasets/fd1d618ac3174ad5be730524a4dd778e_26.geojson');
+    // markers.forEach(marker => {
+    //   addMarker(marker.lat, marker.lon, marker.contentString, map);
+    // });
+
+
 }).catch(function (error) {
   console.error(error)
 })
 
 
-  let treePromiseObject = new TreePromise();
-  let treePromise = treePromiseObject.getTrees();
-
-  treePromise.then(function(response){
-    let body = JSON.parse(response);
-    let data = new TreeData();
-
-    for(let i = 0; i < body.features.length; i++){
-      let treesLat = body.features[i].geometry.coordinates[0];
-      let treesLong = body.features[i].geometry.coordinates[1];
-      
-      data.addTreeToTreeData(treesLat, treesLong);
-
-    }
-    // let tree = new Tree(treesLat, treesLong)
-
-
-    console.log(data)
-
-    // console.log(trees);
-
-  })
 
 })
